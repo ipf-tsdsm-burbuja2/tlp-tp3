729070d2a3762aa2abc5b4f566ddf0a8bef92912
ivanvrdev <ivangaston20032017@gmail.com>
2021-09-10 16:37:44 -0300

    validación

--- controllers/nodos.controllers.js
+++ controllers/nodos.controllers.js
@@ -5,11 +5,15 @@ let ipCome = '';
 
 ctrlNodos.routeGo = async (req, res)=>{
     const nodos = req.body;
-    res.json({msg: 'recibido!'});
-    const ipSend = '192.168.80.233';
-    ipCome = nodos[nodos.length-1].msg.ip;
-    let arrayNodos = addArray(nodos, ipSend, myIp);
-    sendArray(arrayNodos, ipSend);
+    console.log(nodos);
+    if (nodos[nodos.length-1].password == 1955) {
+        res.json({msg: 'recibido!'});
+        const ipSend = '192.168.80.233';
+        ipCome = nodos[nodos.length-1].msg.ip;
+        let arrayNodos = addArray(nodos, ipSend, myIp);
+        sendArray(arrayNodos, ipSend);
+    }
+    res.json({msg: 'Acceso denegado!'});
 }
 
 ctrlNodos.routeCome = async (req, res)=>{
@@ -18,25 +22,5 @@ ctrlNodos.routeCome = async (req, res)=>{
     nodos.pop();
     sendArray(nodos, ipCome);
 }
-
-// const addArray = (nodos, ipSend, myIp) =>{
-//     const misDatos = {"msg":{
-//             "ip": myIp,
-//             "name": "D"},
-//         "ip-send": ipSend,
-//         "ip-recibe": nodos[nodos.length-1].msg.ip}
-//     const arrayNodos = [...nodos, misDatos];
-//     return arrayNodos;
-// }
-
-// const sendArray = async (arrayNodos, ipSend) =>{
-//     const response = await fetch(`http://${ipSend}:4000`, {
-//         method: 'post',
-//         body: JSON.stringify({nodos: arrayNodos}),
-//         headers: {'Content-Type': 'application/json'}
-//     });
-//     const data = await response.json();
-//     console.log(data);
-// }
 
 module.exports = ctrlNodos;
--- controllers/utils.js
+++ controllers/utils.js
@@ -2,12 +2,6 @@ const fetch = require('node-fetch');
 const utils = {};
 
 utils.addArray = (nodos, ipSend, myIp) =>{
-    // const misDatos = {"msg":{
-    //         "ip": myIp,
-    //         "name": "D"},
-    //     "ip-send": ipSend,
-    //     "ip-recibe": nodos[nodos.length-1].msg.ip,
-    //     "password": "1955"};
     const misDatos = {
         msg: {
             ip: myIp,
@@ -17,19 +11,15 @@ utils.addArray = (nodos, ipSend, myIp) =>{
         "ip-recibe": nodos[nodos.length-1].msg.ip,
         password: "1955"
     }
-    // const arrayNodos = [...nodos, misDatos];
-    // console.log(arrayNodos);
-    // return arrayNodos;
     nodos.push(misDatos)
     return nodos;
 }
 
 utils.sendArray = async (arrayNodos, ipSend) =>{
-    console.log(arrayNodos);
     try{
         const response = await fetch(`http://${ipSend}:4000`, {
             method: 'post',
-            body: arrayNodos,
+            body: JSON.stringify(arrayNodos),
             headers: {'Content-Type': 'application/json'}
         });
         const data = await response.json();
